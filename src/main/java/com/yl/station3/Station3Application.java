416910efa40a5ffbe2ee9d74545d774bae9cdbdd
moon maria <tatujjang@gmail.com>
2025-06-14 11:59:48 +0900

    init

---
+++ src/main/java/com/yl/station3/Station3Application.java
@@ -0,0 +1,13 @@
+package com.yl.station3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Station3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Station3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/yl/station3/Station3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yl.station3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Station3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
